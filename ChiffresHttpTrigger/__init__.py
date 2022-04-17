44ccd08bc58d6267ec481c74961aeaf366b711a9
Luc Vo Van <luc.vovan@outlook.com>
2022-04-18 01:20:28 +0200

    Added Chiffres

--- ChiffresHttpTrigger/__init__.py
+++ ChiffresHttpTrigger/__init__.py
@@ -1,24 +1,33 @@
+import re
 import logging
-
+import json
+from py_expression_eval import Parser
+from shared_code import hand
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    formula = req.get_json().get("formula")
+    h = hand.hand()
+    # Check if formula contains bag numbers
+    logging.info(formula)
+    numbersInHand = h["chiffres"]["l"]
+    matches = re.finditer(r"(\d+)", formula)
+    numbersUsed = 0
+    for match in matches:
+        number = int(match.group())
+        if number not in numbersInHand:
+            return func.HttpResponse(f"Invalid move: {number} not in hand {numbersInHand}.", status_code=400)
+        numbersInHand.remove(number)
+        numbersUsed += 1
+        logging.info(f" > {numbersInHand}")
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    parser = Parser()
+    f = parser.parse(formula)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    targetNumber = int(h["chiffres"]["n"])
+    res = {}
+    res["delta"] = abs(int(h["chiffres"]["n"]) - f.evaluate({}))
+    res["numbersUsed"] = numbersUsed
+
+    return func.HttpResponse(json.dumps(res))
---
+++ shared_code/hand.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import random
+import json
+import pathlib
+
+def chiffres():
+    bag = []
+    for i in range(1, 10):
+        bag.append(i)
+    bag += [25, 50, 75, 100]
+    hand = []
+    random.seed(a=datetime.utcnow().strftime("%Y%m%d"), version=2)
+    for i in range(0, 6):        
+        hand.append(bag[random.randrange(0, len(bag))])
+    return random.randint(101, 999), hand
+
+def lettres(lang, letterCount, minVowels, minConsonants):
+    vowels = ["A", "E", "I", "O", "U", "Y"]
+    consonantsBag = []
+    vowelsBag = []
+    with open(str(pathlib.Path(__file__).parent) + "/" + lang + '.json') as f:
+        data = json.load(f)
+    distribution = data["TileSet"]["Distribution"]
+    for k in distribution:
+        if k != "*":
+            if k in vowels:
+                for i in range(0, distribution[k]):
+                    vowelsBag.append(k)
+            else:
+                for i in range(0, distribution[k]):
+                    consonantsBag.append(k)
+
+    hand = []
+
+    random.seed(a=datetime.utcnow().strftime("%Y%m%d"), version=2)
+    # random.seed(a=datetime.utcnow().strftime("%Y%m%d%H%M%s"), version=2)
+    for i in range(0, minVowels):
+        vowel = vowelsBag[random.randrange(0, len(vowelsBag))]
+        hand.append(vowel)
+        vowelsBag.remove(vowel)
+    for i in range(0, minConsonants):
+        consonant = consonantsBag[random.randrange(0, len(consonantsBag))]
+        hand.append(consonant)
+        consonantsBag.remove(consonant)
+    bag = vowelsBag + consonantsBag
+    for i in range(0, letterCount - minVowels - minConsonants):
+        letter = bag[random.randrange(0, len(bag))]
+        hand.append(letter)
+        bag.remove(letter)
+
+    random.shuffle(hand)
+    return hand
+
+def hand():
+    hand = {}
+    c = chiffres()
+    hand["chiffres"] = { "n":  c[0], "l": c[1]}
+    hand["lettres"] = lettres("fr", 9, 2, 2)
+    return hand
